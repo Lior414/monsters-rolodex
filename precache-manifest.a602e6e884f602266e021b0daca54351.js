d3d5314806499d121e62b1c0fa3ead8f4ab070e7
Lior414 <ngrin.lior@gmail.com>
2020-09-14 13:49:54 +0300

    Updates

---
+++ precache-manifest.a602e6e884f602266e021b0daca54351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd4229f60e4fa0190257ae599c328b3f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "67882d04696facc58652",
+    "url": "/monsters-rolodex/static/css/main.8170a20f.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67882d04696facc58652",
+    "url": "/monsters-rolodex/static/js/main.a58133ad.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
