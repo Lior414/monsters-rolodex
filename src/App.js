4f1c9b5cd0a0e1474875c42b81b8c80dc1438372
Lior414 <ngrin.lior@gmail.com>
2020-09-14 13:51:47 +0300

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(){ //constructor() is a method that runs first in the DOM and will be used to set up the component
+    super(); //super() is a method the extends the scope of the component to the rest of the app
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount(){ //componentDidMount() is lifecycle method. lifecycle methods are functions that can reference the state of a component and alter it
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
+  render(){ // render() is a lifecycle method which will create the HTML elements of a component
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter( monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    return(
+      <div className="App">
+        <h1>Monsters Rolodex</h1>    
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters}/>        
+      </div>
+    )
+  };
 }
 
 export default App;
